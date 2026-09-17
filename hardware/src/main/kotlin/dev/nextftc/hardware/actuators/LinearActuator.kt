4e43eac4033ee92c5e2dfc7fecc896e8c1f6e4d1
Roushil Satta <roushil.satta@gmail.com>
2026-09-16 22:05:40 -0700

    fix: setters and getters

--- hardware/src/main/kotlin/dev/nextftc/hardware/actuators/LinearActuator.kt
+++ hardware/src/main/kotlin/dev/nextftc/hardware/actuators/LinearActuator.kt
@@ -7,6 +7,7 @@ import dev.nextftc.units.measuretypes.Distance
 import dev.nextftc.units.measuretypes.Per
 import dev.nextftc.units.unittypes.AngleUnit
 import dev.nextftc.units.unittypes.DistanceUnit
+import kotlin.math.roundToInt
 
 /**
  * Wraps a `NextMotor` and a distance-per-rotation, to work on linear units
@@ -15,25 +16,37 @@ import dev.nextftc.units.unittypes.DistanceUnit
  * @param distPerRotation linear distance traveled per one motor rotation
  */
 class LinearActuator(val motor: NextMotor, val distPerRotation: Per<DistanceUnit, AngleUnit>) {
-  fun setPosition(distance: Distance) {
-    // convert distance to rotations
-    val rotations = distance.into(Inches) / distPerRotation.magnitude
-    val angle: Angle = Rotations.of(rotations)
-    motor.setPositionSetpoint(angle)
-  }
+  var position: Distance
+    get() = Inches.of(motor.encoderPosition.into(Rotations) * distPerRotation.magnitude)
+    set(value) {
+      // convert distance to rotations
+      val rotations = value.into(Inches) / distPerRotation.magnitude
+      val angle: Angle = Rotations.of(rotations)
+      motor.setPositionSetpoint(angle)
+    }
 
-  fun setPositionInches(inches: Double) = setPosition(Inches.of(inches))
+  var positionInches: Double
+    get() = position.into(Inches)
+    set(value) {
+      position = Inches.of(value)
+    }
 
-  fun setPositionTicks(ticks: Int) {
-    val angle = (motor.anglePerCount * ticks) as Angle
-    motor.setPositionSetpoint(angle)
-  }
+  var positionTicks: Int
+    get() = ((motor.encoderPosition / motor.anglePerCount).magnitude).roundToInt()
+    set(value) {
+      val angle = (motor.anglePerCount * value) as Angle
+      motor.setPositionSetpoint(angle)
+    }
 
-  fun setThrottle(throttle: Double) {
-    motor.throttle = throttle
-  }
+  var throttle: Double
+    get() = motor.throttle
+    set(value) {
+      motor.throttle = value
+    }
 
-  fun setThrottleContinuous(throttle: Double) {
-    motor.throttle = throttle
-  }
+  var throttleContinuous: Double
+    get() = throttle
+    set(value) {
+      throttle = value
+    }
 }
