4dab3010df10e7a326672c0cd8fedda354eeb017
28shettr <28shettr@elmbrookstudents.org>
2026-09-21 17:35:34 -0500

    Pedro stuff added

---
+++ robot/src/main/kotlin/dev/nextftc/robot/pedro/PedroHook.kt
@@ -0,0 +1,20 @@
+/*
+ * Copyright (c) 2026 NextFTC Team
+ *
+ *  Use of this source code is governed by an BSD-3-clause
+ *  license that can be found in the LICENSE.md file at the root of this repository or at
+ *  https://opensource.org/license/bsd-3-clause.
+ */
+
+package dev.nextftc.robot.pedro
+
+import dev.nextftc.robot.opmode.OpModeHook
+object PedroHook : OpModeHook {
+  override fun afterPeriodic() {
+    Pedro.follower?.update()
+  }
+
+  override fun afterEnd() {
+    Pedro.follower = null
+  }
+}
